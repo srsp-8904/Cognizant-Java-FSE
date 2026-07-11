208595ae226668ae7b0683f5fa16a18ae4f65caa
srsp-8904 <somisettyrangasaipraneeth@gmail.com>
2026-07-12 00:32:11 +0530

    Added week 3 and week 4 files

---
+++ week3/module7/spring-learn/src/main/java/com/cognizant/controller/CountryController.java
@@ -0,0 +1,24 @@
+package com.cognizant.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cognizant.model.Country;
+import com.cognizant.service.CountryService;
+
+@RestController
+public class CountryController {
+
+    @Autowired
+    private CountryService countryService;
+
+    @GetMapping("/countries/{code}")
+    public Country getCountry(@PathVariable String code) {
+
+        return countryService.getCountry(code);
+
+    }
+
+}
